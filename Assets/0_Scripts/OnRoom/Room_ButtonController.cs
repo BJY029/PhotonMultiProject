d1b8b4546b47bada573a39eb0c5ad81a0382e923
BJY029 <go333dragon@gmail.com>
2025-06-26 22:10:34 +0900

    Update Start Game
    게임 시작 버튼이 눌리면, 역할이 부여되고, 게임 씬으로 이동된다.
    
    자세한 사항은 해당 포스트 참고
    https://blog.naver.com/codiary29/223912933894

--- Assets/0_Scripts/OnRoom/Room_ButtonController.cs
+++ Assets/0_Scripts/OnRoom/Room_ButtonController.cs
@@ -1,6 +1,8 @@
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -24,5 +26,56 @@ public class Room_ButtonController : MonoBehaviour
 
         //로컬 플레이어의 CustomProperties를 서버에 업데이트 한다.
         PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+    }
+
+
+    public void StartGame()
+    {
+        //MasterClient만 역할을 부여한다.
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        //플레이어 리스트를 받아온다.
+        var players = PhotonNetwork.PlayerList.ToList();
+        //해당 플레이어들을 랜덤으로 섞는다.
+        Shuffle(players);
+
+        //첫 번째에 위치한 플레이어를 seeker(술래)로 선정한다.
+        Player seeker = players[0];
+
+        //플레이어 리스트를 돌면서
+        foreach (var player in players)
+        {
+            //Seeker에 해당되는 플레이어는 Seeker, 아닌 나머지는 Runner로 역할을 Hashtable에 저장한다.
+            string role = (player == seeker) ? "Seeker" : "Runner";
+
+            var props = new ExitGames.Client.Photon.Hashtable
+            {
+                { "Role", role }
+            };
+
+            //로컬 플레이어릐 CustomProperties를 서버에 업데이트 한다.
+            player.SetCustomProperties(props);
+        }
+
+        //게임 씬으로 이동한다.
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+    //Fisher-Yates Shuffle 알고리즘
+    public void Shuffle(IList list)
+    {
+        //랜덤 객체 생성
+        System.Random rand = new System.Random();
+        //리스트의 요소 개수를 파악
+        int n = list.Count;
+
+        //마지막 하나가 남을 때까지
+        while(n > 1)
+        {
+            //랜덤한 위치와 임의의 위치를 swap 한다.
+            n--;
+            int k = rand.Next(n + 1); //0 ~ n
+            (list[k], list[n]) = (list[n], list[k]);
+        }
     }
 }
--- Assets/0_Scripts/Photon_Manager.cs
+++ Assets/0_Scripts/Photon_Manager.cs
@@ -4,50 +4,13 @@ using Photon.Realtime;
 
 public class Photon_Manager : MonoBehaviourPunCallbacks
 {
-	[Tooltip("방의 최대 플레이어 참가 수.")]
-	[SerializeField]
-	private byte maxPlayersPerRoom = 4;
-
     private string gameVersion = "1";
 
 	public GameObject camera;
 
-	//해당 설정을 통해 PhotonNetwork.LoadLevel() 호출 시,
-	//마스터 클라이언트가 씬을 로드하면, 해당 방에 있는 다른 클라이언트들도 같은 씬을 로드하게 만든다.
-	private void Awake()
-	{
-		PhotonNetwork.AutomaticallySyncScene = true;
-	}
-
 	private void Start()
 	{
-		Connect();
-	}
-
-	public void Connect()
-	{
-		//이미 Photon 서버에 연결되어 있는 경우
-		if (PhotonNetwork.IsConnected)
-		{
-			//랜던 방 입장을 시도한다.
-			PhotonNetwork.JoinRandomRoom();
-		}
-		else //서버에 아직 연결되지 않은 경우
-		{
-			//버전을 설정하고
-			PhotonNetwork.GameVersion = gameVersion;
-			//서버 접속을 시도한다.
-			PhotonNetwork.ConnectUsingSettings();
-		}
-	}
-
-	//Master 서버에 연결된 경우
-	public override void OnConnectedToMaster()
-	{
-		Debug.Log("포톤 마스터 서버에 연결하였습니다.");
-
-		//랜덤 방에 참여 시도
-		PhotonNetwork.JoinRandomRoom();
+		SpawnPlayer();
 	}
 
 	//연결 끊어진 경우
@@ -56,22 +19,6 @@ public class Photon_Manager : MonoBehaviourPunCallbacks
 		Debug.Log("다음의 이유로 서버 연결이 해제되었습니다 : " + cause);
 	}
 
-	//방 참가에 실패한 경우
-	public override void OnJoinRandomFailed(short returnCode, string message)
-	{
-		Debug.Log("방 참가에 실패했습니다. 방을 새로 만듭니다.");
-
-		//새로운 방을 생성한다.
-		PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom});
-	}
-
-	//방에 정상적으로 접속한 경우
-	public override void OnJoinedRoom()
-	{
-		Debug.Log("룸에 접속하였습니다.");
-		//해당 방에 플레이어 스폰
-		SpawnPlayer();
-	}
 
 	//MasterClient가 변경되면 호출되는 함수
 	public override void OnMasterClientSwitched(Player newMasterClient)
